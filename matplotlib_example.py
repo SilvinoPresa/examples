9dcf458d39acff1596532c070ca0f794b4ec6b64
Silvino J. A. Presa <silvinojose.antunapresa@gmail.com>
2015-08-11 13:54:33 +0100

    Matplotlib example with a cuadratic line
    This is just an easy example of how to use matplotlib with Python

---
+++ matplotlib_example.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt #import matplotlib library
+
+x=[-3,-2,-1,0,1,2,3]    #X values
+y=[9,4,1,0,1,4,9]       #Y values
+
+plt.plot(x,y, 'b-')     #instruction to create the plot with x, v and a blue line
+plt.grid(True)          #show a grid on the plot
+plt.axis([-3,3,0,9])    #set axis limits in the form: x0,x1,y0,y1
+plt.title('Test: Cuadratic Line') #title of the plot
+plt.xlabel('X values')  #Label for the X values
+plt.ylabel('Y values')  #Label for the Y vlaues
+plt.show()              #Command to show the plot
